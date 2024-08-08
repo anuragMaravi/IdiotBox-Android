507c1ed5aaf3605b8f5670f7fc6430f817a52b0b
DipenChellani1110 <chellanidipen11@gmail.com>
2024-08-08 00:33:04 -0700

    Add login button in fragment profile

--- app/src/main/java/com/anuragmaravi/idiotbox/ui/fragments/profile/ProfileFragment.java
+++ app/src/main/java/com/anuragmaravi/idiotbox/ui/fragments/profile/ProfileFragment.java
@@ -1,10 +1,12 @@
 package com.anuragmaravi.idiotbox.ui.fragments.profile;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.fragment.app.Fragment;
 import com.anuragmaravi.idiotbox.R;
 
 public class ProfileFragment extends Fragment {
@@ -12,6 +14,17 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button loginButton = view.findViewById(R.id.button_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Login button pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
+
